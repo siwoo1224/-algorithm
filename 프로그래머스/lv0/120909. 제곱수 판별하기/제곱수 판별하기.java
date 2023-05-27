07bf1acd4780b872601eec039f6c09ba0f6c5777
choi si woo <96398475+siwoo1224@users.noreply.github.com>
2023-05-28 01:25:55 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.02 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.java
+++ 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.java
@@ -7,5 +7,12 @@ class Solution {
         }else{
             return 2;
         }
+        
+        //다른풀이
+        /*int answer = 2;
+        for (int i = 1; i < n; i++) {
+            if (n%i == 0 && n == i*i) answer = 1;
+        }
+        return answer;*/
     }
 }
