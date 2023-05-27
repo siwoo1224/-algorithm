b7e1722f0873b320d309dc7e4caa1daebbcce02b
choi si woo <96398475+siwoo1224@users.noreply.github.com>
2023-05-27 20:05:04 +0900

    [level 0] Title: 모음 제거, Time: 11.46 ms, Memory: 75.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+         for (int i = 0; i<my_string.length(); i++){
+            if(my_string.charAt(i) == 'u'){
+                continue;
+            }else if (my_string.charAt(i) == 'a'){
+                continue;
+            }else if (my_string.charAt(i) == 'e'){
+                continue;
+            }else if (my_string.charAt(i) == 'i'){
+                continue;
+            }else if (my_string.charAt(i) == 'o'){
+                continue;
+            }
+            answer += my_string.charAt(i);
+        }
+        return answer;
+    }
+}
