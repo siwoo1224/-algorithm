6d40199135144d0661749b3f7e4c71208880445e
choi si woo <96398475+siwoo1224@users.noreply.github.com>
2023-05-30 14:22:25 +0900

    [level 1] Title: x만큼 간격이 있는 n개의 숫자, Time: 0.03 ms, Memory: 74.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12954. x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.java
@@ -0,0 +1,11 @@
+class Solution {
+    public long[] solution(int x, int n) {
+        long[] answer = new long[n];
+        long a = x;
+        for(int i = 0; i<n; i++){
+            answer[i] = a;
+            a += x;
+        }
+        return answer;
+    }
+}
