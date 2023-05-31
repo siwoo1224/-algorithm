b8a0fcbdc582fc548b7412f0ace0fedcc3624c3d
choi si woo <96398475+siwoo1224@users.noreply.github.com>
2023-05-31 22:47:58 +0900

    [unrated] Title: 문자열 바꿔서 찾기, Time: 2.45 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
+++ 프로그래머스/unrated/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
@@ -1,6 +1,7 @@
 class Solution {
     public int solution(String myString, String pat) {
         String answer = "";
+        // myString = myString.replace("A", "a").replace("B", "A").replace("a", "B");
         for (int i = 0; i<myString.length(); i++){
             if(myString.charAt(i) == 'A'){
                 answer += "B";
@@ -9,5 +10,6 @@ class Solution {
             }
         }
         return answer.contains(pat) ? 1:0;
+        
     }
 }
