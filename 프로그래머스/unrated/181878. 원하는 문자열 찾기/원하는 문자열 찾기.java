870a6862bb4f304df01aadc9709e608659013082
choi si woo <96398475+siwoo1224@users.noreply.github.com>
2023-05-31 21:54:29 +0900

    [unrated] Title: 원하는 문자열 찾기, Time: 7.66 ms, Memory: 81.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181878. 원하는 문자열 찾기/원하는 문자열 찾기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(String myString, String pat) {
+        int answer = 0;
+        myString = myString.toLowerCase();
+        pat = pat.toLowerCase();
+        System.out.println(myString);
+        System.out.println(pat);
+        if(myString.contains(pat)){
+            answer = 1;
+        }else {
+            answer = 0;
+        }
+        return answer;
+    }
+}
