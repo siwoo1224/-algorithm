ffcffd91565a2f2535192f26322b1efe989b313e
choi si woo <96398475+siwoo1224@users.noreply.github.com>
2023-06-17 11:54:18 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 261.58 ms, Memory: 96.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+class Solution {
+    public String solution2(String[] participant, String[] completion) {
+        // 1. 두 배열을 정렬한다
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+
+        // 2. 두 배열이 다를 때까지 찾는다
+        int i = 0;
+        for(i=0;i<completion.length;i++)
+            if(!participant[i].equals(completion[i]))
+                break;
+
+
+        // 3. 여기까지 왔다는 것은 마지막 주자가 완주하지 못했다는 의미이다.
+        return participant[i];
+    }
+    
+    static public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+
+        for (int i = 0; i < completion.length; i++) {
+            if(!participant[i].contains(completion[i])){
+                return participant[i];
+            }
+        }
+        return participant[participant.length -1];
+    }
+}
